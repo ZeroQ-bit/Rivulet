a598a813048f7a81d2146da4986818417d9664a3
Bain Gurley <baingurley@gmail.com>
2026-04-21 19:58:23 -0500

    media: introduce MediaUserState

---
+++ Rivulet/Models/Media/MediaUserState.swift
@@ -0,0 +1,15 @@
+//
+//  MediaUserState.swift
+//  Rivulet
+//
+//  Per-user playback/library state for a media item.
+//
+
+import Foundation
+
+struct MediaUserState: Hashable, Sendable {
+    let isPlayed: Bool
+    let viewOffset: TimeInterval     // seconds; 0 if not started
+    let isFavorite: Bool             // Plex starred / Jellyfin favorite
+    let lastViewedAt: Date?
+}
